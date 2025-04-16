1e8714465fadea830ca720eac07be92c7fcb79e0
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-04-15 22:45:59 -0300

    ajustando

--- server_with_fastify/controllers/middleware.js
+++ server_with_fastify/controllers/middleware.js
@@ -7,14 +7,14 @@ export const verifyToken = async (req, reply) => {
     const token = req.cookies.token;
     //console.log(req.cookies.token);
     if (!token) {
-      console.log("não contem token");
+      //console.log("não contem token");
       return reply.redirect("/login/admin");
     }
 
     // Verifica o token JWT
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // Adiciona os dados do usuário à requisição
-    console.log("Verificação ok");
+    //console.log("Verificação ok");
   } catch (err) {
     console.error("Erro ao verificar token:", err.message);
     return reply.code(401).send("Token inválido ou expirado");
