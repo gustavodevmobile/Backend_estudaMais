0524da810ba15c66bc3c5e71b5a5bbecfb4b47a2
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-04-22 10:33:16 -0300

    Update cadastrar_questao.js

--- server_with_fastify/controllers/cadastrar_questao.js
+++ server_with_fastify/controllers/cadastrar_questao.js
@@ -15,7 +15,7 @@ export const cadastrar_questao = async function (req, reply) {
   const imagePath = path.resolve(__dirname, "../images/" + req.file.filename);
   let image;
   let filename;
-  //console.log("req.file.filename", req.file.filename);
+  console.log("req.file.filename", req.file.filename);
   if (!req.file || !req.file.filename) {
     req.body.image = "sem imagem";
     image = req.body.image;
