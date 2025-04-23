2ed7b3e143a7112e6ea32a182fc1848906e26eca
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-04-23 09:52:16 -0300

    ajustes

--- server_with_fastify/controllers/cadastrar_questao.js
+++ server_with_fastify/controllers/cadastrar_questao.js
@@ -1,4 +1,4 @@
-import fs, { readFileSync } from "fs";
+import fs from "fs";
 
 import "dotenv/config";
 import Database from "../models/Questions.js";
@@ -12,46 +12,13 @@ export const screen_cadastrar_questao = async (req, reply) => {
 };
 
 export const cadastrar_questao = async function (req, reply) {
-  let image;
-  let filename;
-
   console.log("req.file.filname", req.file.filename);
   console.log("req.file", req.file);
   if (!req.file || !req.file.filename) {
     req.body.image = "sem imagem";
-    image = req.body.image;
-    filename = null;
-  } else {
-    try {
-      //const file = await req.file();
-      const imageDir = path.resolve(__dirname, "../images/");
+    const image = req.body.image;
+    const filename = null;
 
-      if (!fs.existsSync(imageDir)) {
-        fs.mkdirSync(imageDir, { recursive: true });
-        console.log("Diretório criado:", imageDir);
-      } else {
-        console.log("Diretório já foi criado:", imageDir);
-      }
-
-      const imagePath = path.join(imageDir, req.file.originalname);
-
-      fs.writeFileSync(imagePath, req.file.originalname);
-      const buffer = readFileSync(imagePath); // Use req.file.buffer para salvar o arquivo
-      console.log("buffer", buffer);
-      image = buffer;
-      console.log("Imagem salva:", imagePath);
-
-      // Use o buffer da imagem
-
-      filename = req.file.filename;
-    } catch (e) {
-      console.error(`Erro ao salvar a imagem: ${e}`);
-      return reply.code(500).send({ error: `Erro ao salvar iamgem ${e}` });
-    }
-  }
-  // Obtenha o arquivo enviado
-
-  try {
     await Database.create({
       elementarySchool: req.body.elementarySchool,
       schoolYear: req.body.schoolYear,
@@ -66,10 +33,51 @@ export const cadastrar_questao = async function (req, reply) {
       alternativeC: req.body.altC,
       alternativeD: req.body.altD,
     });
-    console.log("Cadastrado com sucesso");
+    console.log("Questão sem imagem salva com sucesso:");
     return reply.redirect("/");
-  } catch (err) {
-    console.log(err);
-    return reply.send(err);
+  } else {
+    try {
+      //const file = await req.file();
+      const imageDir = path.resolve(__dirname, "../images/");
+
+      if (!fs.existsSync(imageDir)) {
+        fs.mkdirSync(imageDir, { recursive: true });
+        console.log("Diretório criado:", imageDir);
+      } else {
+        console.log("Diretório já foi criado:", imageDir);
+      }
+
+      const imagePath = path.join(imageDir, req.file.filename);
+
+      fs.writeFileSync(imagePath, req.file.filename);
+      const buffer = fs.readFileSync(imagePath); // Use req.file.buffer para salvar o arquivo
+      console.log("buffer", buffer);
+      const image = buffer;
+      console.log("Imagem salva:", imagePath);
+
+      // Use o buffer da imagem
+
+      const filename = req.file.filename;
+      await Database.create({
+        elementarySchool: req.body.elementarySchool,
+        schoolYear: req.body.schoolYear,
+        displice: req.body.displice,
+        subject: req.body.subject,
+        question: req.body.question,
+        image: image,
+        nameImageDir: filename,
+        answer: req.body.answer,
+        alternativeA: req.body.altA,
+        alternativeB: req.body.altB,
+        alternativeC: req.body.altC,
+        alternativeD: req.body.altD,
+      });
+      console.log("Questão com imagem salva com sucesso:");
+      return reply.redirect("/");
+    } catch (e) {
+      console.error(`Erro ao salvar a imagem: ${e}`);
+      return reply.code(500).send({ error: `Erro ao salvar iamgem ${e}` });
+    }
   }
+  // Obtenha o arquivo enviado
 };
--- server_with_fastify/controllers/home.js
+++ server_with_fastify/controllers/home.js
@@ -7,31 +7,36 @@ export const home = async (req, reply) => {
     let nameImage = [];
     await Database.findAll().then((result) => {
       result.map((element) => {
-        const imageBase64 = element["dataValues"]["image"].toString("base64");
-        element["dataValues"]["image"] = imageBase64;
-        nameImage.push(element["dataValues"]["nameImageDir"]);
-        console.log(element);
+       // console.log(element.dataValues.image);
+        const imageBase64 = element.dataValues.image.toString("base64");
+          element.dataValues.image = imageBase64;
+          nameImage.push(element.dataValues.nameImageDir);
+        // if (element.dataValues.image) {
+          
+        // } else {
+        //   console.log('element.dataValues.image recebeu nulo');
+        // }
       });
-      
+
       return reply.render("home", {
         question: result,
         length: result.length,
       });
     });
     //console.log("nameImage", nameImage);
-    fs.readdir("./images/", { withFileTypes: true }, (err, files) => {
-      files.map((el) => {
-        if (nameImage.includes(el.name)) {
-          //console.log('existe', el.name)
-        } else {
-          fs.unlinkSync("./images/" + el.name);
-        }
-      });
-      if (err) {
-        console.log("Erro ao ler arquivo", err);
-        return;
-      }
-    });
+    // fs.readdir("../images/", { withFileTypes: true }, (err, files) => {
+    //   files.map((el) => {
+    //     if (nameImage.includes(el.name)) {
+    //       //console.log('existe', el.name)
+    //     } else {
+    //       fs.unlinkSync("../images/" + el.name);
+    //     }
+    //   });
+    //   if (err) {
+    //     console.log("Erro ao ler arquivo", err);
+    //     return;
+    //   }
+    // });
   } catch (err) {
     console.log(err);
     return reply.send(err);
