1ade82629397c7b7b921f356c6ddbcd1f292a5fc
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-05-02 20:00:12 -0300

    feedback

--- server_with_fastify/controllers/feedbacks.js
+++ server_with_fastify/controllers/feedbacks.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 
-export const sendToEmail = async (req, reply) => {
+export const sendToEmailFeedbackfailures = async (req, reply) => {
   const { message, descriptions } = req.body;
   try {
     const transporter = nodemailer.createTransport({
@@ -19,13 +19,40 @@ export const sendToEmail = async (req, reply) => {
       html: `<h1>Feedback de Erro em questão</h1>
              <p>${message}</p>
              <h2>Descrição do Erro</h2>
-             <p>${descriptions} </p>`,
+             <p>${ descriptions }</p>`,
     };
-    const result = await transporter.sendMail(mailOptions);
-    reply.code(200).send("Feedback enviado com sucesso! Obrigado!");
+    await transporter.sendMail(mailOptions);
+    reply.code(200).send("Feedback da questão enviado com sucesso! Obrigado!");
     //console.log("E-mail enviado com sucesso!", result);
   } catch (error) {
-    reply.code(500).send("Algo deu errado ao enviar o feedback: " + error.message);
+    reply.code(500).send("Algo deu errado ao enviar o feedback da questão: " + error.message);
+    console.error("Erro ao enviar e-mail:", error.message);
+  }
+};
+
+export const sendToEmailFeedbackApp = async (req, reply) => {
+  const { message } = req.body;
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: "gucorrea.dev@outlook.com",
+      subject: "Novo feedback recebido",
+      text: "Feedback sobre o App",
+      html: `<h1>Feedback sobre o App</h1>
+             <p>${message}</p>`,
+    };
+    await transporter.sendMail(mailOptions);
+    reply.code(200).send("Feedback do app enviado com sucesso! Obrigado!");
+    
+  } catch (error) {
+    reply.code(500).send("Algo deu errado ao enviar o feedback do app: " + error.message);
     console.error("Erro ao enviar e-mail:", error.message);
   }
 };
--- server_with_fastify/routes/routes_feedbacks/routes.js
+++ server_with_fastify/routes/routes_feedbacks/routes.js
@@ -1,8 +1,8 @@
-import { sendToEmail } from "../../controllers/feedbacks.js";
+import { sendToEmailFeedbackfailures, sendToEmailFeedbackApp  } from "../../controllers/feedbacks.js";
 
 async function route_feedback(fastify, options) {
-  fastify.post("/feedback", sendToEmail);
-    
+  fastify.post("/feedback", sendToEmailFeedbackfailures);  
+  fastify.post("/feedback-app", sendToEmailFeedbackApp); 
 }
 
 export default route_feedback;
